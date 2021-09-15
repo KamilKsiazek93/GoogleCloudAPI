68e323ed2ab11f0500427b593c153511f3c0a883
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-09-15 09:31:30 +0200

    Added interface and class for connection with secret api key

---
+++ Data/GoogleService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace GoogleCloudServiceAPI.Data
+{
+    public class GoogleService : IGoogleService
+    {
+        private readonly string GOOGLE_SPEECH_KEY;
+        private readonly string GOOGLE_TRANSLATE_KEY;
+
+        public GoogleService(IConfiguration configuration)
+        {
+            GOOGLE_SPEECH_KEY = configuration["GoogleCloudSecretKey:Speech"];
+            GOOGLE_TRANSLATE_KEY = configuration["GoogleCloudSecretKey:Translate"];
+        }
+        public string GetSpeechWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTranaslateWord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
